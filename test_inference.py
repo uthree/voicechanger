1710de3797e6bbb85284f2b58cd89c4e85c0e031
uthree <uthree271828@gmail.com>
2023-04-25 01:01:07 +0900

    fix

--- test_inference.py
+++ test_inference.py
@@ -1,4 +1,15 @@
 import sys
+import json
 
 sys.path.append("./hifigan")
+from inference import load_checkpoint
 from models import Generator
+from env import AttrDict
+
+# Load Generator
+checkpoint_dict = load_checkpoint("./g_02500000", 'cpu')
+with open('./hifigan/config_v1.json') as f:
+    json_config = json.loads(f.read())
+h = AttrDict(json_config)
+generator = Generator(h)
+generator.load_state_dict(checkpoint_dict['generator'])
