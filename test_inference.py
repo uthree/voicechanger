f5cb1f6afba50445ce73219c08cb530c917cae5b
uthree <uthree271828@gmail.com>
2023-04-25 14:48:11 +0900

    fix

--- test_inference.py
+++ test_inference.py
@@ -3,20 +3,26 @@ import json
 import torchaudio
 import os
 import glob
-from spectrogram import linear_spectrogram, linear_to_mel, plot_spectrogram
+from spectrogram import linear_spectrogram, linear_to_mel
+from cyclegan import Generator as Convertor
+import torch
 
 sys.path.append("./hifigan")
 from inference import load_checkpoint
-from models import Generator
+from models import Generator as Vocoder
 from env import AttrDict
 
-# Load Generator
+# Load Vocoder
 checkpoint_dict = load_checkpoint("./g_02500000", 'cpu')
 with open('./hifigan/config_v1.json') as f:
     json_config = json.loads(f.read())
 h = AttrDict(json_config)
-generator = Generator(h)
-generator.load_state_dict(checkpoint_dict['generator'])
+vocoder = Vocoder(h)
+vocoder.load_state_dict(checkpoint_dict['generator'])
+
+# Load convertor
+convertor = Convertor()
+convertor.load_state_dict(torch.load("./g_a2b.pt", map_location='cpu'))
 
 if not os.path.exists("./outputs/"):
     os.mkdir("./outputs")
@@ -26,8 +32,8 @@ for i, path in enumerate(paths):
     wf, sr = torchaudio.load(path)
     wf = torchaudio.functional.resample(wf, sr, 22050)
     lin_spec = linear_spectrogram(wf)
-    plot_spectrogram(lin_spec[0], f"./outputs/{i}.png")
+    lin_spec = convertor(lin_spec)
     spec = linear_to_mel(lin_spec)
-    wf = generator(spec)
+    wf = vocoder(spec)
     wf = torchaudio.functional.resample(wf, 22050, sr)[0]
     torchaudio.save(filepath=os.path.join("./outputs/", f"{i}.wav"), src=wf, sample_rate=sr)
