1b6d3bd84e408025111722b28f58123b0b0068a2
uthree <uthree271828@gmail.com>
2023-04-25 14:32:27 +0900

    added train_cyclegan.py

---
+++ spectrogram.py
@@ -0,0 +1,57 @@
+import torch
+from librosa.filters import mel as librosa_mel_fn
+import matplotlib.pyplot as plt
+
+mel_basis = {}
+hann_window = {}
+
+
+def dynamic_range_compression_torch(x, C=1, clip_val=1e-5):
+    return torch.log(torch.clamp(x, min=clip_val) * C)
+
+
+def spectral_normalize_torch(magnitudes):
+    output = dynamic_range_compression_torch(magnitudes)
+    return output
+
+
+def linear_spectrogram(y, n_fft=1024, num_mels=80, sampling_rate=22050, hop_size=256, win_size=1024, fmin=0, fmax=8000, center=False):
+    if torch.min(y) < -1.:
+        print('min value is ', torch.min(y))
+    if torch.max(y) > 1.:
+        print('max value is ', torch.max(y))
+
+    global mel_basis, hann_window
+    if fmax not in mel_basis:
+        mel = librosa_mel_fn(sr=sampling_rate, n_fft=n_fft, n_mels=num_mels, fmin=fmin, fmax=fmax)
+        mel_basis[str(fmax)+'_'+str(y.device)] = torch.from_numpy(mel).float().to(y.device)
+        hann_window[str(y.device)] = torch.hann_window(win_size).to(y.device)
+
+    y = torch.nn.functional.pad(y.unsqueeze(1), (int((n_fft-hop_size)/2), int((n_fft-hop_size)/2)), mode='reflect')
+    y = y.squeeze(1)
+
+    spec = torch.stft(y, n_fft, hop_length=hop_size, win_length=win_size, window=hann_window[str(y.device)],
+                      center=center, pad_mode='reflect', normalized=False, onesided=True, return_complex=True).abs()
+
+    # spec = torch.sqrt(spec.pow(2).sum(-1)+(1e-9))
+    return spec
+
+
+def linear_to_mel(spec, n_fft=1024, num_mels=80, sampling_rate=22050, hop_size=256, win_size=1024, fmin=0, fmax=8000, center=False):
+    global mel_basis, hann_window
+    if fmax not in mel_basis:
+        mel = librosa_mel_fn(sr=sampling_rate, n_fft=n_fft, n_mels=num_mels, fmin=fmin, fmax=fmax)
+        mel_basis[str(fmax)+'_'+str(spec.device)] = torch.from_numpy(mel).float().to(spec.device)
+        hann_window[str(spec.device)] = torch.hann_window(win_size).to(spec.device)
+
+    spec = torch.matmul(mel_basis[str(fmax)+'_'+str(spec.device)], spec)
+    spec = spectral_normalize_torch(spec)
+    return spec
+
+# Plot spectrogram
+def plot_spectrogram(x, save_path="./spectrogram.png", log=True):
+    if log:
+        x = torch.log10(x ** 2 + 1e-4)
+    plt.imshow(x)
+    plt.savefig(save_path, dpi=200)
+
--- test_inference.py
+++ test_inference.py
@@ -3,12 +3,12 @@ import json
 import torchaudio
 import os
 import glob
+from spectrogram import linear_spectrogram, linear_to_mel, plot_spectrogram
 
 sys.path.append("./hifigan")
 from inference import load_checkpoint
 from models import Generator
 from env import AttrDict
-from meldataset import mel_spectrogram
 
 # Load Generator
 checkpoint_dict = load_checkpoint("./g_02500000", 'cpu')
@@ -25,7 +25,9 @@ paths = glob.glob("./inputs/*.wav")
 for i, path in enumerate(paths):
     wf, sr = torchaudio.load(path)
     wf = torchaudio.functional.resample(wf, sr, 22050)
-    spec = mel_spectrogram(wf, 1024, 80, 22050, 256, 1024, 0, 8000)
+    lin_spec = linear_spectrogram(wf)
+    plot_spectrogram(lin_spec[0], f"./outputs/{i}.png")
+    spec = linear_to_mel(lin_spec)
     wf = generator(spec)
     wf = torchaudio.functional.resample(wf, 22050, sr)[0]
     torchaudio.save(filepath=os.path.join("./outputs/", f"{i}.wav"), src=wf, sample_rate=sr)
