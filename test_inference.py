fcd6235c71ce1b179c53c941b4f76335e4353655
uthree <uthree271828@gmail.com>
2023-04-27 19:42:28 +0900

    fix

--- test_inference.py
+++ test_inference.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 import json
 import torchaudio
@@ -12,17 +13,29 @@ from inference import load_checkpoint
 from models import Generator as Vocoder
 from env import AttrDict
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--device', default='cpu')
+parser.add_argument('-mp', '--model-path', default='./g_a2b.pt')
+parser.add_argument('-ig', '--input-gain', default=1.0, type=float)
+parser.add_argument('-g', '--gain', default=1.0, type=float)
+
+args = parser.parse_args()
+
+device = torch.device(args.device)
+
 # Load Vocoder
-checkpoint_dict = load_checkpoint("./g_02500000", 'cpu')
+checkpoint_dict = load_checkpoint("./g_02500000", device)
 with open('./hifigan/config_v1.json') as f:
     json_config = json.loads(f.read())
 h = AttrDict(json_config)
 vocoder = Vocoder(h)
+vocoder = vocoder.to(device)
 vocoder.load_state_dict(checkpoint_dict['generator'])
 
 # Load convertor
 convertor = Convertor()
-convertor.load_state_dict(torch.load("./g_a2b.pt", map_location='cpu'))
+convertor.load_state_dict(torch.load(args.model_path, map_location=device))
+convertor = convertor.to(device)
 
 if not os.path.exists("./outputs/"):
     os.mkdir("./outputs")
@@ -30,14 +43,14 @@ if not os.path.exists("./outputs/"):
 paths = glob.glob("./inputs/*.wav")
 for i, path in enumerate(paths):
     wf, sr = torchaudio.load(path)
-    wf = torchaudio.functional.resample(wf, sr, 22050)
+    wf = wf.to(device)
+    wf = torchaudio.functional.resample(wf, sr, 22050) * args.input_gain
     with torch.no_grad():
+        print(f"convertiong {path}")
         lin_spec = linear_spectrogram(wf)
-        plot_spectrogram(lin_spec[0], f"./outputs/{i}_input.png")
         lin_spec = convertor(lin_spec)
-        plot_spectrogram(lin_spec[0], f"./outputs/{i}_output.png")
         spec = linear_to_mel(lin_spec)
     wf = vocoder(spec)
-    wf = torchaudio.functional.resample(wf, 22050, sr)[0]
-    wf = wf.detach()
+    wf = torchaudio.functional.resample(wf, 22050, sr)[0] * args.gain
+    wf = wf.cpu().detach()
     torchaudio.save(filepath=os.path.join("./outputs/", f"{i}.wav"), src=wf, sample_rate=sr)
