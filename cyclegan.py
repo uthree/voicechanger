295c85006cebca780ea79ed4e3815f3816c9b538
uthree <uthree271828@gmail.com>
2023-05-08 03:07:14 +0900

    fix

--- cyclegan.py
+++ cyclegan.py
@@ -4,11 +4,11 @@ import torch.nn.functional as F
 
 
 class ResBlock(nn.Module):
-    def __init__(self, channels=256, spectral_norm=True, alpha=0.01, dropout=0):
+    def __init__(self, channels=256, spectral_norm=True, alpha=0.01, dropout=0, groups=1):
         super().__init__()
-        self.c1 = nn.Conv1d(channels, channels, 5, 1, 2)
+        self.c1 = nn.Conv1d(channels, channels, 5, 1, 2, groups=groups)
         self.act = nn.LeakyReLU(alpha)
-        self.c2 = nn.Conv1d(channels, channels, 5, 1, 2)
+        self.c2 = nn.Conv1d(channels, channels, 5, 1, 2, groups=groups)
         self.dropout_rate = dropout
         if spectral_norm:
             self.c1 = torch.nn.utils.spectral_norm(self.c1)
@@ -24,11 +24,11 @@ class ResBlock(nn.Module):
 
 
 class Generator(nn.Module):
-    def __init__(self, input_channels=513, internal_channels=256, num_layers=7):
+    def __init__(self, input_channels=513, internal_channels=256, num_layers=7, groups=1):
         super().__init__()
         self.input_layer = nn.Conv1d(input_channels, internal_channels, 1, 1, 0)
         self.mid_layers = nn.Sequential(
-                *[ResBlock(internal_channels, False) for _ in range(num_layers)])
+                *[ResBlock(internal_channels, False, groups=groups) for _ in range(num_layers)])
         self.output_layer = nn.Conv1d(internal_channels, input_channels, 1, 1, 0)
 
     def forward(self, x):
@@ -39,12 +39,12 @@ class Generator(nn.Module):
 
 
 class Discriminator(nn.Module):
-    def __init__(self, input_channels=513, internal_channels=256, num_layers=6):
+    def __init__(self, input_channels=513, internal_channels=256, num_layers=6, groups=1):
         super().__init__()
         self.input_layer = nn.utils.spectral_norm(
                 nn.Conv1d(input_channels, internal_channels, 5, 1, 0))
         self.mid_layers = nn.Sequential(
-                *[ResBlock(internal_channels, True, dropout=0.0, alpha=0.2) for _ in range(num_layers)])
+                *[ResBlock(internal_channels, True, groups=groups, dropout=0.0, alpha=0.2) for _ in range(num_layers)])
         self.output_layer = nn.Conv1d(internal_channels, 1, 1, 1, 0)
 
     def forward(self, x):
